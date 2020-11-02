fa59688722815a73f365c66c9f307ee093d6a92f
Alexsandr Vobornov <vobos2@gmail.com>
2020-11-01 22:32:30 -0500

    Updates

---
+++ precache-manifest.8a422de65a897e8f886672e5e68ab003.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a78812562347b2cd1e90b900aac545d",
+    "url": "/Salon2.0/index.html"
+  },
+  {
+    "revision": "0ee469b0e047faa00df0",
+    "url": "/Salon2.0/static/css/2.733aa2fb.chunk.css"
+  },
+  {
+    "revision": "146592aa2fb29de05e0a",
+    "url": "/Salon2.0/static/css/main.6a0f41b7.chunk.css"
+  },
+  {
+    "revision": "0ee469b0e047faa00df0",
+    "url": "/Salon2.0/static/js/2.e068f9fd.chunk.js"
+  },
+  {
+    "revision": "ce60d7cd1657e1ad0033aec17e4dbcda",
+    "url": "/Salon2.0/static/js/2.e068f9fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "146592aa2fb29de05e0a",
+    "url": "/Salon2.0/static/js/main.ce15d9c1.chunk.js"
+  },
+  {
+    "revision": "4a48bf0c7c9e305916ae",
+    "url": "/Salon2.0/static/js/runtime-main.031400a1.js"
+  },
+  {
+    "revision": "36477da985421f329140301881519fbf",
+    "url": "/Salon2.0/static/media/bg.36477da9.jpg"
+  }
+]);
