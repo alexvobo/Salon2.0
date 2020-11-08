589de27d3bdec8427d4c2c5e17b3e16950aa980a
Alexsandr Vobornov <vobos2@gmail.com>
2020-11-08 07:42:32 -0500

    modified env variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+require('dotenv').config()
+
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -9,6 +11,5 @@ const app = firebase.initializeApp({
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
-console.log(app.apiKey);
 export const auth = app.auth();
 export default app;
