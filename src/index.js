a372bbe76ba0c53713cb4d2a1939c6d47f430ff1
Alexsandr Vobornov <vobos2@gmail.com>
2020-11-08 08:07:23 -0500

    changed location of dotenv import

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
+require("dotenv").config();
 
 ReactDOM.render(
   <React.StrictMode>
